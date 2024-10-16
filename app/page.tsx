58ef2960aa11b91e5a97cec72a2020ebae1d351a
lagooneration <puneetkumarlagoo@gmail.com>
2024-10-17 02:29:09 +0530

    Intro page

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ export default function Dashboard() {
   // }
 
   return (
+    <div className="w-full h-full flex flex-row justify-center items-center p-10 mt-10">
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
@@ -32,16 +33,7 @@ export default function Dashboard() {
             </p>
           </div>
           <form  className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="picture">Location</Label>
-              <Input
-                id="picture"
-                type="file"
-                placeholder="location"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
+          <div className="grid gap-2">
               <Label htmlFor="name">Name</Label>
               <Input
                 id="name"
@@ -49,6 +41,39 @@ export default function Dashboard() {
                 placeholder="Property Name"
                 required
               />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="picture">Location</Label>
+              <div className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50">
+              <div className="relative">
+                <input
+                  type="file"
+                  id="name"
+                  className="hidden"
+                  required
+                />
+                <label
+                  htmlFor="name"
+                  className="flex items-center justify-center cursor-pointer"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 mr-2"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                    />
+                  </svg>
+                  <span className="text-muted-foreground">Upload File</span>
+                </label>
+              </div>
+              </div>
             </div>
             <div className="grid gap-2">
               <div className="flex items-center">
@@ -66,7 +91,7 @@ export default function Dashboard() {
           </form>
           <div className="mt-4 text-center text-sm">
             Haven&apos;t traded carbon yet?{" "}
-            <Link href="https://www.citizencorrects.com" className="underline">
+            <Link href="https://www.citizencorrects.com" className="underline text-blue-500">
               Citizen Corrects
             </Link>
           </div>
@@ -74,13 +99,14 @@ export default function Dashboard() {
       </div>
       <div className="hidden bg-muted lg:block">
         <Image
-          src="/intro.png"
+          src="/assets/intro.png"
           alt="Image"
           width="1920"
           height="1080"
           className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
+    </div>
     </div>
   )
 }
--- components/ui/month.tsx
+++ components/ui/month.tsx
@@ -20,7 +20,6 @@ export function Month() {
     <div className="grid gap-3">
       <Label htmlFor="month">Month: <span className="text-blue-500">{monthNames[month - 1]}</span></Label>
       <div className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50">
-
       <Slider
         id="month"
         defaultValue={[5]}
